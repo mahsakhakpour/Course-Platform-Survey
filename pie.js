9d68dd385bccd0333a4bd4ca259954543152c658
Mahsa <mahsa54@gmail.com>
2026-01-06 23:24:09 -0800

    Update pie.js

--- pie.js
+++ pie.js
@@ -1,11 +1,35 @@
 function highcharts_pie(data) {
     Highcharts.chart('container', {
-        chart: { type: 'pie' },
-        title: { text: 'BCIT Course Platform Preferences' },
+        chart: {
+            type: 'pie'
+        },
+        title: {
+            text: 'BCIT Course Platform Preferences'
+        },
+        tooltip: {
+            pointFormat: '{point.label}'
+        },
+        plotOptions: {
+            pie: {
+                allowPointSelect: false,
+                cursor: 'pointer',
+                dataLabels: {
+                    enabled: true,
+                    format: '{point.label}',
+                    distance: -30,
+                    style: {
+                        fontWeight: 'bold',
+                        color: 'white'
+                    }
+                },
+                showInLegend: false
+            }
+        },
         series: [{
             name: 'Votes',
             colorByPoint: true,
-            data: data
+            data: data,
+            colors: ['#7cb5ec', '#434348', '#90ed7d'] // Different colors for each segment
         }]
     });
 }
